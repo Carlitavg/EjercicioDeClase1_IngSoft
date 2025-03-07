72ed91e909dbf2ba3cf9e4f6a6f858b48bf24933
Carla_Diana <carladianavgonzales@gmail.com>
2025-03-06 23:27:59 -0400

    feat: devuelve el 6.25% de impuesto para el esttado de 'TX'

--- src/impuesto_por_estado.js
+++ src/impuesto_por_estado.js
@@ -2,6 +2,9 @@ function impuesto(codigo_estado) {
     if(codigo_estado == "NV"){
         return 8.00
     }
+    if(codigo_estado == "TX"){
+        return 6.25
+    }
     return 6.65
   }
   
--- src/impuesto_por_estado.test.js
+++ src/impuesto_por_estado.test.js
@@ -7,4 +7,7 @@ describe("Impuesto por estado", () => {
   it("para el codigo de estado 'NV' deberia devolver el porcentaje 8.00%", () => {
     expect(impuesto("NV")).toEqual(8.00);
   });
+  it("para el codigo de estado 'TX' deberia devolver el porcentaje 6.25%", () => {
+    expect(impuesto("TX")).toEqual(6.25);
+  });
 });

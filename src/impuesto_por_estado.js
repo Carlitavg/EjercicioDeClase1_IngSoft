f6a910a283d5aa1ae01bff4601f0da285829a715
christian-ledezma <chris.ledezma.s@gmail.com>
2025-03-07 01:10:02 -0400

    Fix: Cambio de nombre a variable

--- src/impuesto_por_estado.js
+++ src/impuesto_por_estado.js
@@ -1,5 +1,5 @@
 export function impuesto(codigo_estado) {
-    const descuentos = {
+    const impuesto = {
         UT: 6.65,
         NV: 8.00,
         TX: 6.25,
@@ -7,7 +7,7 @@ export function impuesto(codigo_estado) {
         CA: 8.25
     };
     
-    return descuentos[codigo_estado]
+    return impuesto[codigo_estado]
     
   }
 

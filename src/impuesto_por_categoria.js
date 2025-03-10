fcdb59c2f87f8f337de0df696e749da309c63a1d
christian-ledezma <chris.ledezma.s@gmail.com>
2025-03-09 20:23:06 -0400

    Refactor: Cambio de la logica para devolver el impuesto por categoria

--- src/impuesto_por_categoria.js
+++ src/impuesto_por_categoria.js
@@ -1,6 +1,7 @@
 export function get_impuesto_por_categoria(codigo_producto){
-    let impuesto = 0; 
-    if(codigo_producto == 'Bebidas alcohólicas')
-      impuesto = 7;
-    return impuesto;
+    const impuesto = {
+        'Alimentos': 0,
+        'Bebidas alcohólicas': 7
+    }
+    return impuesto[codigo_producto];
 }

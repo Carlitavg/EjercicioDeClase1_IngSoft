2f077d47e418f3cf8f2a2b64b1b8a7bade5a6958
Carla_Diana <carladianavgonzales@gmail.com>
2025-03-10 23:10:31 -0400

    Feat: funcionalidad para el tipo de cliente 'Recurrente' se aplica 0.5% de descuento en el envio

--- src/descuento_envio_por_tipoCliente.test.js
+++ src/descuento_envio_por_tipoCliente.test.js
@@ -4,4 +4,7 @@ describe("Descuento en el costo de envio por el tipo de Cliente", () => {
   it("para el tipo de cliente 'Normal' se deberia aplicar 0% de descuento en el envio", () => {
     expect(get_descuento_tipoCliente("Normal")).toEqual(0);
   }); 
+  it("para el tipo de cliente 'Recurrente' se deberia aplicar 0.5% de descuento en el envio", () => {
+    expect(get_descuento_tipoCliente("Recurrente")).toEqual(0.5);
+  }); 
 }); 

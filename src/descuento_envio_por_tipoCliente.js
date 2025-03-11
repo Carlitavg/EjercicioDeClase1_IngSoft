512a9a86d790bc0c08c2c457105fb36dd3855d75
Carla_Diana <carladianavgonzales@gmail.com>
2025-03-10 23:13:30 -0400

    Feat: funcionalidad para el tipo de cliente 'Especial' se aplica 1.5% de descuento en el envio

--- src/descuento_envio_por_tipoCliente.js
+++ src/descuento_envio_por_tipoCliente.js
@@ -1,4 +1,5 @@
 function get_descuento_tipoCliente(tipo_cliente) {
+    if(tipo_cliente == "Especial") return 1.5;
     if(tipo_cliente == "Antiguo Recurrente") return 1;
     if(tipo_cliente == "Recurrente") return 0.5;
     return 0;
--- src/descuento_envio_por_tipoCliente.test.js
+++ src/descuento_envio_por_tipoCliente.test.js
@@ -10,4 +10,7 @@ describe("Descuento en el costo de envio por el tipo de Cliente", () => {
   it("para el tipo de cliente 'Antiguo Recurrente' se deberia aplicar 1% de descuento en el envio", () => {
     expect(get_descuento_tipoCliente("Antiguo Recurrente")).toEqual(1);
   }); 
+  it("para el tipo de cliente 'Especial' se deberia aplicar 1.5% de descuento en el envio", () => {
+    expect(get_descuento_tipoCliente("Especial")).toEqual(1.5);
+  }); 
 }); 

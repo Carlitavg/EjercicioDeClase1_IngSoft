9e896f392dbed6454e5e389d2f5dfb9cd92f63fe
christian-ledezma <chris.ledezma.s@gmail.com>
2025-03-11 00:36:39 -0400

    Feat: Calcular el descuento de beneficio por tipo de cliente Especial, venta neta > 7000 y categoria de producto Electronicos

--- src/descuento_por_beneficio.js
+++ src/descuento_por_beneficio.js
@@ -1,9 +1,14 @@
 export function get_descuento_beneficio(tipo_cliente, venta_neta, categoria_productos){
     if (
+        tipo_cliente == 'Especial' &&
+        venta_neta > 7000 &&
+        categoria_productos == 'Electrónicos')
+        return 200;
+    else if (
         tipo_cliente == 'Recurrente' &&
         venta_neta > 3000 &&
         categoria_productos == 'Alimentos')
-    return 100;
+        return 100;
     else
         return 0;
 }
--- src/descuento_por_beneficio.test.js
+++ src/descuento_por_beneficio.test.js
@@ -4,7 +4,10 @@ describe("Descuento por tipo de cliente, precio neto y categoria de producto", (
   it("para cliente recurrente, precio neto > 3000 y Alimentos", () => {
     expect(get_descuento_beneficio("Recurrente", 3500, "Alimentos")).toEqual(100);
   }); 
-  it("para cliente recurrente, precio neto > 3000 y Alimentos", () => {
+  it("para cliente recurrente, precio neto < 3000 y Alimentos", () => {
     expect(get_descuento_beneficio("Recurrente", 2800, "Alimentos")).toEqual(0);
+  }); 
+  it("para cliente especial, precio neto > 7000 y Electronicos", () => {
+    expect(get_descuento_beneficio("Especial", 7100, "Electrónicos")).toEqual(200);
   }); 
 }); 
